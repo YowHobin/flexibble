da85df3f9bfe41d9d07f8d829039c14d3a1ca805
YowHobin <lruarellano@rtu.edu.ph>
2023-08-07 17:46:35 +0800

    pls

--- next.config.js
+++ next.config.js
@@ -6,10 +6,8 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   }
 };
 
